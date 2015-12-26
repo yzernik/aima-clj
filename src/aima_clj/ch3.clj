831ad9b077169552eeaa74557597fe44b4e09963
yzernik <yzernik@gmail.com>
2015-12-26 15:39:21 -0800

    fix partial successor function

--- src/aima_clj/ch3.clj
+++ src/aima_clj/ch3.clj
@@ -4,14 +4,14 @@
   "An abstract formulation of a search problem"
   (initial-state [this] "Initial state in which the agent starts")
   (actions [this state] "Possible actions available to the agent at a state")
-  (result [this state action] "A description of what each action does")
-  (is-goal? [this state] "Determines whether a given state is a goal state")
-  (step-cost [this state action] "The cost of taking an action in a state to reach a new state"))
+  (result [this state action] "The result of taking an action at a state")
+  (goal? [this state] "Determines whether a given state is a goal state")
+  (step-cost [this state action] "The cost of taking an action in a state"))
 
 (defprotocol Fringe
   "A strategy for inserting and removing nodes from a fringe"
   (insert [this node] "Insert a new node into the fringe")
-  (remove-next [this] "Remove the next node from the fring"))
+  (remove-next [this] "Remove the next node from the fringe"))
 
 (defrecord Node [state path cost])
 
@@ -27,7 +27,7 @@
   "The successor nodes of a given node for a problem"
   [problem node]
   (let [a (actions problem (:state node))]
-    (map (partial problem node) a)))
+    (map (partial successor problem node) a)))
 
 (defn tree-search
   "General tree search algorithm"
@@ -36,6 +36,6 @@
     (loop [f (insert fringe start)]
       (if-not (empty? f)
         (let [[node f] (remove-next f)]
-          (cond (is-goal? problem (:state node)) (:path node)
+          (cond (goal? problem (:state node)) (:path node)
                 :else (let [s (successors problem node)]
                         (recur (reduce insert f s)))))))))
